567426aa61baa42b06a65629702c44436576eccd
kirill <k.anisimov@bag24.ru>
2024-04-24 21:34:49 +0300

    fix: crossorigin

--- app/app.py
+++ app/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api import init_routers
 from app.dependencies.webapp import WebAppContainer
@@ -6,7 +7,21 @@ from app.dependencies.webapp import WebAppContainer
 
 def create_app() -> FastAPI:
     WebAppContainer()
+
     app = FastAPI()
+
+    origins = [
+        "*",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,  # type: ignore[call-arg]
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
     init_routers(app)
 
     return app
